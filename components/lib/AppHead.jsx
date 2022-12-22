1eec7fa992c2b4f31ccb40cad85719a68708aba9
anointedngeorge <anointedngeorge@gmail.com>
2022-12-22 02:06:38 +0100

    c3456579

--- components/lib/AppHead.jsx
+++ components/lib/AppHead.jsx
@@ -12,6 +12,8 @@ export const AppHead = ({title='Bomach'}) => {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content='true' />
         <meta name="author" content='true' />
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
         <title>
             {title}
         </title>
