bf639ecb40016d16ea62ba8b149d5aed7c60f341
anointedngeorge <anointedngeorge@gmail.com>
2022-12-23 22:06:15 +0100

    c233456

--- functions.jsx
+++ functions.jsx
@@ -254,6 +254,9 @@ export async function settings_form(el) {
       method: 'POST',
       headers: {
         'Content-Type':'application/json',
+        "Access-Control-Allow-Origin":"*",
+        "Access-Control-Allow-Methods":"DELETE, POST, GET, OPTIONS",
+        "Access-Control-Allow-Headers":"Content-Type, Authorization, X-Requested-With"
       },
       data: data
     }).then(data => {
