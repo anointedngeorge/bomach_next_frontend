e118d8cd7df84ca2cda75ae9b39164e8fb99c852
anointedngeorge <anointedngeorge@gmail.com>
2022-12-27 16:48:37 +0100

    c3433434

--- components/layout/Topnav.jsx
+++ components/layout/Topnav.jsx
@@ -6,7 +6,7 @@ export const Topnav = () => {
   return (
     <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
     {/* Navbar Brand*/}
-    <a className="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
+    <a className="navbar-brand ps-3" href="index.html">BomachGroup</a>
     {/* Sidebar Toggle*/}
     <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i className="fas fa-bars" /></button>
     {/* Navbar Search*/}
